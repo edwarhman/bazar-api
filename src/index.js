cd8be83f688c38ec360f8419fe280efbeab65163
Emerson Warhman <edwarhman@gmail.com>
2023-09-30 14:14:01 -0400

    add morgan package for log requests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { createProductsRouter } from './routes/products.js'
 import 'dotenv/config.js'
+import morgan from 'morgan'
 
 const app = express()
 const port = process.env.PORT ?? 3000
 
 export const createApp = ({ productModel }) => {
+  app.use(morgan('tiny'))
   app.get('/', (req, res) => {
     res.send('hello World')
   })
